21af9603f2844460e239a4de40c1caa93331f142
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2019-01-18 11:21:37 +0000

    # SSKTS-1119 ### Changed - 人物インターフェースを拡張

--- src/factory/person.ts
+++ src/factory/person.ts
@@ -1,61 +1,26 @@
-import PersonType from './personType';
-import * as ProgramMembershipFactory from './programMembership';
-import { IPropertyValue } from './propertyValue';
+import * as cinerino from '@cinerino/factory';
+
+import { IProgramMembership } from './programMembership';
 
 /**
  * プロフィールインターフェース
  */
-export interface IProfile {
-    /**
-     * Given name. In the U.S., the first name of a Person. This can be used along with familyName instead of the name property.
-     */
-    givenName: string;
-    /**
-     * Family name. In the U.S., the last name of an Person. This can be used along with givenName instead of the name property.
-     */
-    familyName: string;
-    /**
-     * The telephone number.
-     */
-    telephone: string;
-    /**
-     * Email address.
-     */
-    email: string;
-}
+export type IProfile = cinerino.person.IProfile;
+
 /**
  * @alias
  * @deprecated Use IProfile instead.
  */
 export type IContact = IProfile;
+
 /**
  * 識別子インターフェース
  */
-export type IIdentifier = IPropertyValue<string>[];
+export type IIdentifier = cinerino.person.IIdentifier;
+
 /**
  * 人物インターフェース
  */
-export interface IPerson {
-    /**
-     * type of object
-     */
-    typeOf: PersonType;
-    /**
-     * person id (Amazon Cognito User Identifier)
-     */
-    id: string;
-    /**
-     * An Organization (or ProgramMembership) to which this Person or Organization belongs.
-     * 所属会員プログラム
-     */
-    memberOf?: ProgramMembershipFactory.IProgramMembership;
-    /**
-     * URL of the item.
-     */
-    url?: string;
-    /**
-     * 人を識別するもの
-     * サービスを使用するアプリケーション側で都合のいいように設定する
-     */
-    identifier?: IIdentifier;
+export interface IPerson extends cinerino.person.IPerson {
+    memberOf?: IProgramMembership;
 }
