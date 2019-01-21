67670389baa96e554132e4e13e5e700c4d2c4fac
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2019-01-21 05:46:00 +0000

    # SSKTS-1119 ### Changed - 決済方法タイプを拡張

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -11,6 +11,7 @@ The format is based on [Keep a Changelog](http://keepachangelog.com/).
 ### Changed
 
 - 人物インターフェースを拡張
+- 決済方法タイプを拡張
 
 ### Deprecated
 
--- src/factory/paymentMethodType.ts
+++ src/factory/paymentMethodType.ts
@@ -1,10 +1,32 @@
 /**
  * 決済方法タイプ
  */
-
 enum PaymentMethodType {
+    /**
+     * 現金
+     */
+    Cash = 'Cash',
+    /**
+     * 口座決済
+     * @deprecated Use PaymentMethodType.Account
+     */
+    Pecorino = 'Account',
+    /**
+     * 口座決済
+     */
+    Account = 'Account',
+    /**
+     * 電子マネー
+     */
+    EMoney = 'EMoney',
+    /**
+     * クレジットカード決済
+     */
     CreditCard = 'CreditCard',
-    Pecorino = 'Pecorino'
+    /**
+     * ムビチケ
+     */
+    MovieTicket = 'MovieTicket'
 }
 
 export default PaymentMethodType;
